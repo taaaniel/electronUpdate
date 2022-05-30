280dfb6a8e3463c7f0b1d5c4ba31df8009beec0a
Wojciech Kaliszewski <wkaliszewski@sii.pl>
2022-05-30 13:09:41 +0200

    add checking for update.

--- public/electron.js
+++ public/electron.js
@@ -74,7 +74,20 @@ autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
     detail:
       "A new version has been downloaded. Restart the application to apply the updates.",
   };
+
   dialog.showMessageBox(dialogOpts).then((returnValue) => {
     if (returnValue.response === 0) autoUpdater.quitAndInstall();
   });
+});
+
+autoUpdater.on("checking-for-update", (_event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Restart", "Later"],
+    title: "Application Update",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail:
+      "A new version has been downloaded. Restart the application to apply the updates.",
+  };
+  console.log("checking-for-update", dialogOpts);
 });
--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import version from "../package.json";
+
 function App() {
+  console.log("process.env.REACT_APP_VERSION", version.version);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Autoupdater Demo v{process.env.REACT_APP_VERSION} dupa~1</p>
+        <p>Autoupdater Demo v{version.version} </p>
       </header>
     </div>
   );
